4e5ebe792d8ad3d71db0d798c9db528f2c09e16e
Facundo Cordoba <facucordoba200@gmail.com>
2026-04-12 15:35:59 -0300

    changes for cors

--- src/index.ts
+++ src/index.ts
@@ -14,7 +14,7 @@ app.use(logger())
 app.use(
   "/*",
   cors({
-    origin: ["https://totem.uno", "http://localhost:5173"],
+    origin: ["https://totem.uno", "http://localhost:5173", "https://totem-admin-9hw.pages.dev/login"],
     allowMethods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowHeaders: ["Content-Type", "Authorization"],
     credentials: true,
